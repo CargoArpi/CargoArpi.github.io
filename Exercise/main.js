813a9ca7fad6a5ca04fe6e64bfe7355410bfd607
CargoArpi <hunfifus@gmail.com>
2021-12-12 07:01:44 +0100

    add

--- Exercise/main.js
+++ Exercise/main.js
@@ -1,6 +1,6 @@
 var dDay = new Date()
 var nHours = dDay.getHours()
-if ((nHours <12 ) && (nHours >=5)) {document.write("jó reggelt!")}
-if ((nHours >=12) && (nHours <19)) {document.write("jó napot!")}
+if ((nHours <8 ) && (nHours >=5)) {document.write("<h1>jó reggelt</h1>")}
+if ((nHours >=8) && (nHours <19)) {document.write("jó napot!")}
 if ((nHours >=19) && (nHours <24)) {document.write("jó estét!")}
 if ((nHours >=0) && (nHours <5)) {document.write("jó éjszakát!")}
